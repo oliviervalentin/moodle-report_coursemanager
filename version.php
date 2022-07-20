0f6465bf423f119d74622e55baf2550dfc97fd6d
Olivier VALENTIN <elgjyn@gmail.com>
2022-07-20 12:42:47 +0200

    Update required version
    Update required version

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();                                                                                                
                                                                                                                                                                                                                              
 $plugin->version = 2017022001;                                                                                                                                                                                
-$plugin->requires = 2022041900;                                                                                                                                                                     
+$plugin->requires = 2018051718;                                                                                                                                                                     
 $plugin->component = 'report_coursemanager';       
 $plugin->maturity = 'MATURITY_ALPHA'; 
