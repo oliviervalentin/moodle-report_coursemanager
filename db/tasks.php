5a3c161450ab427d9fdafd4580cd1a984f165a23
Olivier VALENTIN <elgjyn@gmail.com>
2023-06-13 16:25:17 +0200

    Update tasks.php
    Update execution hour (2am instead of every 10 minutes)

--- db/tasks.php
+++ db/tasks.php
@@ -19,7 +19,7 @@
  *
  * @package report_coursemanager
  * @category task
- * @copyright 2023 YOLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+ * @copyright 2023
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
