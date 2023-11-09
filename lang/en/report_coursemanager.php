472940db26e053e66ee6329a7c488800c6a1bb1b
oliviervalentin <elgjyn@gmail.com>
2023-11-09 11:29:50 +0100

    Add privacy provider (no personal data stored in this plugin)

--- lang/en/report_coursemanager.php
+++ lang/en/report_coursemanager.php
@@ -311,3 +311,7 @@ $string['mailingddescreportnostudent'] = 'Il n\'y a aucun étudiant inscrit dans
 $string['mailingddescreportnovisitteacher'] = 'Les cours suivants n\'ont pas enregistré de visite d\'enseignants depuis une longue période';
 $string['mailingddescreportheavy'] = 'Le poids des fichiers rattachés à ce cours est important. Cliquez sur un cours pour afficher la décomposition des fichiers.';
 $string['mailingddescreportorphansubmissions'] = 'Les activités Devoir de ces cours contiennent des dépôts effectués par des étudiants qui ne sont plus inscrits. Ces dépôts invisibles peuvent représenter un poids important !';
+
+
+// Privacy.
+$string['privacy:no_data_reason'] = 'Le plugin Course Manager n\'enregistre aucune donnée personnelle. Il établit des rapports à partir de données non personnelles résultant de l\'étude des contenus des cours (paramétrages, nombre d\'étudiants, nombre d\'activités...).';
