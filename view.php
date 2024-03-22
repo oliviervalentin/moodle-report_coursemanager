a70b263dc1f5b19535a496ddee0adcbd25967061
oliviervalentin <elgjyn@gmail.com>
2024-03-22 10:38:36 +0100

    Debug - Course search in teacher dashboard

--- view.php
+++ view.php
@@ -33,6 +33,7 @@ $done = optional_param('done', 0, PARAM_RAW);
 $site = get_site();
 
 $PAGE = new moodle_page();
+$PAGE->requires->js(new moodle_url($CFG->wwwroot . '/report/coursemanager/search_courses.js'));
 $PAGE->set_context(context_system::instance());
 $PAGE->set_heading(get_string('title', 'report_coursemanager'));
 $PAGE->set_url('/report/coursemanager/view.php');
@@ -81,7 +82,7 @@ unset($done);
 // Buttons to filter lines.
 print('
 <h3>'.get_string('title', 'report_coursemanager').'</h3>
-<input type="text" id="myInput" onkeyup="myFunction()" placeholder="'.get_string('text_filter', 'report_coursemanager').'">
+<input type="text" id="courseInput" onkeyup="searchCourses()" placeholder="'.get_string('text_filter', 'report_coursemanager').'">
 
 <input type="radio" class="tablefilter" name="course_filter" id="filterrow" checked />
 <label for="filterrow" class="btn btn-outline-primary"><i class=\'fa fa-lg fa-list\'></i>
