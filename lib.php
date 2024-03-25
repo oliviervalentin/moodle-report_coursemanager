bd31a17d8a657178686dd93ff90e97d9e56fac25
oliviervalentin <elgjyn@gmail.com>
2024-03-25 10:13:40 +0100

    Code deleted - Useless functions regarding theme

--- lib.php
+++ lib.php
@@ -27,50 +27,6 @@ require_once("$CFG->libdir/formslib.php");
 
 global $COURSE, $OUTPUT, $PAGE, $CFG;
 
-function report_coursemanager_get_main_scss_content($theme) {
-    global $CFG;
-
-    $scss = '';
-    $filename = !empty($theme->settings->preset) ? $theme->settings->preset : null;
-    $fs = get_file_storage();
-
-    $context = context_system::instance();
-    if ($filename == 'default.scss') {
-        // We still load the default preset files directly from the boost theme. No sense in duplicating them.
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
-    } else if ($filename == 'plain.scss') {
-        // We still load the default preset files directly from the boost theme. No sense in duplicating them.
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
-    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'report_coursemanager', 'preset', 0, '/', $filename))) {
-        // This preset file was fetched from the file area for report_coursemanager and not theme_boost (see the line above).
-        $scss .= $presetfile->get_content();
-    } else {
-        // Safety fallback - maybe new installs etc.
-        $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/default.scss');
-    }
-    // Pre CSS - this is loaded AFTER any prescss from the setting but before the main scss.
-    $pre = file_get_contents($CFG->dirroot . '/theme/coursemanager/scss/pre.scss');
-    // Post CSS - this is loaded AFTER the main scss but before the extra scss from the setting.
-    $post = file_get_contents($CFG->dirroot . '/theme/coursemanager/scss/post.scss');
-
-    // Combine them together.
-    return $pre . "\n" . $scss . "\n" . $post;
-}
-
-function theme_navcoursemanager_extend_navigation(global_navigation $root) {
-    $node = navigation_node::create(
-        'Coucou',
-        new moodle_url('/local/greetings/index.php'),
-        navigation_node::TYPE_CUSTOM,
-        null,
-        null,
-        new pix_icon('t/message', '')
-    );
-    $node->showinflatnavigation = true;
-
-    $root->add_node($node);
-}
-
 /**
  * Get comments for assign files.
  *
