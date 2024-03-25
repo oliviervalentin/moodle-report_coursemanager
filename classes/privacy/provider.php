d3645641bb67612338e520980a8ef6a195c1eb88
oliviervalentin <elgjyn@gmail.com>
2024-03-25 15:29:03 +0100

    Moodle Code Checker issue for privacy provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -30,6 +30,7 @@ namespace report_coursemanager\privacy;
  * This plugin does not store any personal user data.
  */
 class provider implements \core_privacy\local\metadata\null_provider {
+
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
