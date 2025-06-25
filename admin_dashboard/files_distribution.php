32770b21de272d7e7703012d7425620052072b5d
oliviervalentin3 <oliviervalentin3@gmail.com>
2025-06-25 09:57:24 +0200

    Add links in admion pages to get back to main menu

--- admin_dashboard/files_distribution.php
+++ admin_dashboard/files_distribution.php
@@ -43,6 +43,8 @@ $PAGE->blocks->add_region('content');
 $PAGE->set_title($site->fullname);
 
 echo $OUTPUT->header();
+$url = new moodle_url('index.php');
+echo $OUTPUT->single_button($url, get_string('back'));
 
 echo html_writer::div(get_string('title_admin_files_distribution', 'report_coursemanager'));
 echo html_writer::div(get_string('admin_files_distribution_info', 'report_coursemanager'));
